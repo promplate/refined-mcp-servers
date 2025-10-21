2018700be231dc17efe012d1bfda03e5b3af2a79
Muspi Merol <me@promplate.dev>
2025-10-21 14:49:37 +0800

    fix: add README to project metadatare

--- packages/gh/gh_mcp/impl.py
+++ packages/gh/gh_mcp/impl.py
@@ -7,7 +7,7 @@ from fastmcp.exceptions import ToolError
 
 from .yaml import readable_yaml_dumps
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 mcp = FastMCP("gh", version=__version__, include_fastmcp_meta=False)
 
